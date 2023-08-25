9752b173e9ed37f1098953f847fe88bfb9856655
thiagoavieira <thiago.vmatos@hotmail.com>
2023-08-25 10:34:19 -0300

    Injetando autenticação

--- IFitness/src/main/java/br/edu/ifsp/arq/dw2s6/ifitness/config/AuthorizationServerConfig.java
+++ IFitness/src/main/java/br/edu/ifsp/arq/dw2s6/ifitness/config/AuthorizationServerConfig.java
@@ -1,10 +1,35 @@
 package br.edu.ifsp.arq.dw2s6.ifitness.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 @Configuration
 @EnableAuthorizationServer
-public class AuthorizationServerConfig {
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory()
+			.withClient("client")
+			.secret("client")
+			.scopes("read", "write")
+			.authorizedGrantTypes("password")
+			.accessTokenValiditySeconds(1800);
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints
+			.tokenStore(tokenStore())
+			.authenticationManager(authenticationManager);
+	}
 }
--- IFitness/src/main/java/br/edu/ifsp/arq/dw2s6/ifitness/config/ResourceServerConfig.java
+++ IFitness/src/main/java/br/edu/ifsp/arq/dw2s6/ifitness/config/ResourceServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -26,4 +27,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 		.and().csrf().disable();
 	}
 	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.stateless(true);
+	}
 }

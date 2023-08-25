8e7886e9eb4a2cb9198b6465a74eb45bd84ba62d
thiagoavieira <thiago.vmatos@hotmail.com>
2023-08-25 10:15:23 -0300

    BasicConfiguration

--- IFitness/src/main/java/br/edu/ifsp/arq/dw2s6/ifitness/config/BasicConfiguration.java
+++ IFitness/src/main/java/br/edu/ifsp/arq/dw2s6/ifitness/config/BasicConfiguration.java
@@ -2,8 +2,10 @@ package br.edu.ifsp.arq.dw2s6.ifitness.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @SuppressWarnings("deprecation")
 @Configuration
@@ -15,7 +17,12 @@ public class BasicConfiguration extends WebSecurityConfigurerAdapter{
 		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ROLE");
 	}
 	
-	public void queroMeMatar() {
-		System.out.println("ao meio dia irei me jogar da ponte");
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated()
+		.and().httpBasic()
+		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().csrf().disable();
 	}
+	
 }

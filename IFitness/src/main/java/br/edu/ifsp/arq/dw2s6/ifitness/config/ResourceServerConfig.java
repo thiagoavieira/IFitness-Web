109f8d1e71ab61fb76fc2e5648318f5971617c97
thiagoavieira <thiago.vmatos@hotmail.com>
2023-08-25 10:22:48 -0300

    Novo adapter

--- IFitness/src/main/java/br/edu/ifsp/arq/dw2s6/ifitness/config/ResourceServerConfig.java
+++ IFitness/src/main/java/br/edu/ifsp/arq/dw2s6/ifitness/config/ResourceServerConfig.java
@@ -1,24 +1,25 @@
 package br.edu.ifsp.arq.dw2s6.ifitness.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
-@SuppressWarnings("deprecation")
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
 @Configuration
 @EnableWebSecurity
-public class BasicConfiguration extends WebSecurityConfigurerAdapter{
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
-	@Override
+	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ROLE");
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)

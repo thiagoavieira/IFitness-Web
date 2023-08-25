702fca336fc1b167aba13ef8ae059d671b6e3504
thiagoavieira <thiago.vmatos@hotmail.com>
2023-08-25 10:28:55 -0300

    Habilitando o servidor

--- IFitness/src/main/java/br/edu/ifsp/arq/dw2s6/ifitness/config/AuthorizationServerConfig.java
+++ IFitness/src/main/java/br/edu/ifsp/arq/dw2s6/ifitness/config/AuthorizationServerConfig.java
@@ -1,5 +1,10 @@
 package br.edu.ifsp.arq.dw2s6.ifitness.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
 public class AuthorizationServerConfig {
 	
 }

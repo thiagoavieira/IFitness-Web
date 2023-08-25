cb96db8545c4391188ed88460fb4f38d400f0af1
thiagoavieira <thiago.vmatos@hotmail.com>
2023-08-25 11:05:17 -0300

    EnableResourceServer

--- IFitness/src/main/java/br/edu/ifsp/arq/dw2s6/ifitness/config/ResourceServerConfig.java
+++ IFitness/src/main/java/br/edu/ifsp/arq/dw2s6/ifitness/config/ResourceServerConfig.java
@@ -7,11 +7,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
 @EnableWebSecurity
+@EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Autowired
